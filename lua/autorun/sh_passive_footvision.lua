b4ab1018fa9eeef87a2b0707262c09310fdcf1f7
yupi2 <yupi2@openmailbox.org>
2015-12-18 01:48:09 -0600

    why the hell did I do any of that before

--- lua/autorun/sh_passive_footvision.lua
+++ lua/autorun/sh_passive_footvision.lua
@@ -2,29 +2,19 @@ if SERVER then
 	AddCSLuaFile("cl_passive_footvision.lua")
 	resource.AddFile("materials/vgui/ttt/icon_footvision.vtf")
 	resource.AddFile("materials/ttt/footprint.vtf")
+else
+	include("cl_passive_footvision.lua")
 end
 
 -- 2048 choosen to prevent any conflicts.
 EQUIP_FOOTVISION = 2048
 
-hook.Add("Initialize", "Setup Passive FootVision", function()
-	local detectiveItems = EquipmentItems[ROLE_DETECTIVE]
-	for k, v in pairs(detectiveItems) do
-		if istable(v) and v.id == EQUIP_FOOTVISION then
-			table.RemoveByValue(detectiveItems, v)
-			break
-		end
-	end
-
-	table.insert(detectiveItems, {
+hook.Add("OnGamemodeLoaded", "Setup Passive FootVision", function()
+	table.insert(EquipmentItems[ROLE_DETECTIVE], {
 		id       = EQUIP_FOOTVISION,
 		type     = "item_passive",
 		material = "vgui/ttt/icon_footvision",
 		name     = "Foot Vision",
 		desc     = "Allows you see footprints."
 	})
-	
-	if CLIENT then
-		include("cl_passive_footvision.lua")
-	end
 end)
